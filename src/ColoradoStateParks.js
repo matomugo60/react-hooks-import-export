1e5481cf60d6b4a2cc65aa685da15b5ca2477476
matomugo60 <matomugo60@gmail.com>
2023-01-10 15:28:07 +0300

    Complete

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,31 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+// function ColoradoStateParks() {
+//   aDifferentName(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+//   return <h1>Colorado State Parks!</h1>;
+// }
+
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
+import { trees, wildlife } from "./parks/RockyMountain";
+
+
+// console.log(RMFunctions.trees);
+
+// RMFunctions.wildlife();
+
+
+// RMFunctions.elevation();
+
+
+
+export default function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
+
